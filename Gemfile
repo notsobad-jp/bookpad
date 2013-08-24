ff81cf7bd5fa304a39263720fdbf1eeed18e2915
tomomichi <tomomichi.onishi@gmail.com>
2013-08-24 12:55:44 +0900

    enable multi store search

--- Gemfile
+++ Gemfile
@@ -7,13 +7,14 @@ gem 'activerecord'
 
 gem 'mechanize'
 gem 'iconv'
-
 gem 'foreman'
 gem 'thin'
-
-gem 'haml'
-gem 'sass'
-gem 'coffee-script'
-gem 'compass'
-
 gem 'newrelic_rpm'
+gem 'haml'
+
+group :development do
+	gem 'pry'
+	gem 'sass'
+	gem 'coffee-script'
+	gem 'compass'
+end
--- main.rb
+++ main.rb
@@ -42,14 +42,7 @@ end
 #検索結果画面
 get '/search/:keyword' do
 	#GoogleBooksAPIで検索
-	@keyword = params[:keyword]
-	encoded_keyword = @keyword
-	encoded_keyword = URI.escape(@keyword)
-	max_result = 6
-	uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{encoded_keyword}&maxResults=#{max_result}&country=JP")
-	json = Net::HTTP.get(uri)
-	@result = JSON.parse(json)
-
+	@result = book_search(params[:keyword], max_result=6)
 	haml :search, :layout => !request.pjax?
 end
 
@@ -69,19 +62,43 @@ end
 #お店の在庫検索(ajaxで呼び出し)
 get '/stock_search/:isbn' do
 	return false if params[:isbn].nil?
+	store_id = 1  #TODO: ログイン情報に応じて検索店舗を変える
+	@stock = stock_search(params[:isbn], store_id)
 
-	mech = Mechanize.new
-	mech.get("https://www.coopbooknavi.jp/zaik/book_search.php")
-	form = mech.page.form_with(:name => "frm")
-	form.field_with(:name => "isbn").value = params[:isbn]
-	form.radiobutton_with(:value => "13036").check
-	form.submit
-	links = mech.page.links
-	@page = Array.new
-	links.each do |link|
-		@page.push(link) if link.href.index("hng")
-	end
-
-	@stock = (@page.empty?) ? nil : @page.join(',')
 	return @stock
+end
+
+
+#キーワードから本を検索
+def book_search(keyword, max_result)
+	encoded_keyword = URI.escape(keyword)
+	uri = URI.parse("https://www.googleapis.com/books/v1/volumes?q=#{encoded_keyword}&maxResults=#{max_result}&country=JP")
+	json = Net::HTTP.get(uri)
+	@result = JSON.parse(json)
+end
+
+
+#在庫検索
+def stock_search(isbn, store_id)
+	case store_id
+	when 1 #東大生協(本郷)
+		mech = Mechanize.new
+		mech.post('https://www.coopbooknavi.jp/zaik/book_search_result.php', {'isbn'=>isbn, "and_or"=>"1", "ten_sel"=>"13036", "search"=>"1"})
+		links = mech.page.links
+		@page = Array.new
+		links.each do |link|
+			@page.push(link) if link.href.index("hng")
+		end
+		@stock = (@page.empty?) ? nil : @page.join(',')
+	when 2 #ブックハウス神保町
+		mech = Mechanize.new
+		mech.get("http://jinbou.books-sanseido.co.jp/jinbou/bookTownJinbou.do?syISBN=#{isbn}")
+		stock_info = mech.page.at("table[3]").at("table[3]").at("tr[5]").at("td[3]").at("p").inner_text
+		@stock = (stock_info.empty?) ? nil : stock_info.gsub(".", ", ")
+	when 3 #岩波ブックセンター
+		mech = Mechanize.new
+		mech.get("http://jinbou.books-sanseido.co.jp/jinbou/bookTownJinbou.do?syISBN=#{isbn}")
+		stock_info = mech.page.at("table[3]").at("table[3]").at("tr[5]").at("td[3]").at("p").inner_text
+		@stock = (stock_info.empty?) ? nil : stock_info.gsub(".", ", ")
+	end
 end
